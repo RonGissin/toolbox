5c7d98ae32649e2957d52971614ecbbc66499108
Ron Gissin <rongissin@microsoft.com>
2024-12-22 15:45:04 +0200

    required statements

--- ConfigGeneration.Tool/Generation/RuntimeConfigurationGenerationOptions.cs
+++ ConfigGeneration.Tool/Generation/RuntimeConfigurationGenerationOptions.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace ToolBox.ConfigGeneration.Tool.Generation
 {
     public record RuntimeConfigurationGenerationOptions
     {
-        public string InputDirectoryPath { get; init; }
+        public required string InputDirectoryPath { get; init; }
 
-        public string OutputDirectoryPath { get; init; }
+        public required string OutputDirectoryPath { get; init; }
 
-        public string HierarchyFilePath { get; init; }
+        public required string HierarchyFilePath { get; init; }
 
-        public List<Assembly> AssembliesToSearch { get; init; }
+        public required List<Assembly> AssembliesToSearch { get; init; }
     }
 }
