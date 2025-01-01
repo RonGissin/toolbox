539a6dc92266aab3f5d414c59a7c31f30123c20d
Ron Gissin <rongissinnew@gmail.com>
2025-01-01 19:30:23 +0200

    removed AddDecorator

--- DependencyInjection/DependencyInjectionExtensions.cs
+++ DependencyInjection/DependencyInjectionExtensions.cs
@@ -9,47 +9,9 @@ namespace ToolBox.DependencyInjection;
 public static class DependencyInjectionExtensions
 {
     /// <summary>
-    /// Registers a decorator for the specified service type.
-    /// The decorated registration lifetime will be defined by the decoratee's defined lifetime.
-    /// Multiple decorators can be added for the same <typeparamref name="TService"/> - they will be applied in the order they are added.
-    /// </summary>
-    /// <typeparam name="TService">The decorated service.</typeparam>
-    /// <typeparam name="TDecorator">The decorator service.</typeparam>
-    /// <param name="services"></param>
-    /// <returns>The service collection.</returns>
-    /// <exception cref="InvalidOperationException">Thrown if <typeparamref name="TService"/> is not yet registered to the service collection.</exception>
-    public static IServiceCollection AddDecorator<TService, TDecorator>(this IServiceCollection services)
-        where TService : class
-        where TDecorator : class, TService
-    {
-        // Find the original service descriptor
-        var decorateeDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(TService));
-        if (decorateeDescriptor == null)
-        {
-            throw new InvalidOperationException($"Service of type {typeof(TService).FullName} not found in the service collection.");
-        }
-
-        // Remove the original descriptor
-        services.Remove(decorateeDescriptor);
-
-        // Add the decorator
-        services.Add(new ServiceDescriptor(typeof(TService), provider =>
-        {
-            // Resolve the original service
-            var originalImplementation = (TService)decorateeDescriptor.ImplementationFactory?.Invoke(provider)
-                                         ?? provider.GetRequiredService(decorateeDescriptor.ImplementationType);
-
-            // Create and return the decorator, passing the original implementation
-            return ActivatorUtilities.CreateInstance<TDecorator>(provider, originalImplementation);
-        }, decorateeDescriptor.Lifetime));
-
-        return services;
-    }
-
-    /// <summary>
     /// Verifies that all services registered in the <paramref name="services"/> can be resolved.
     /// This method must be called last after all registrations have been made, 
-    /// registrations performed afterwards will not be verified.
+    /// registrations performed afterward will not be verified.
     /// </summary>
     /// <param name="services">The populated service collection.</param>
     /// <returns>The verified service provider.</returns>
@@ -64,7 +26,7 @@ public static class DependencyInjectionExtensions
         var verifiableServices = services
             .Where(s => !s.ServiceType.IsGenericTypeDefinition)
             .Where(s => !typeof(IHost).IsAssignableFrom(s.ServiceType))
-            .Where(services => !typeof(IHostEnvironment).IsAssignableFrom(services.ServiceType));
+            .Where(s => !typeof(IHostEnvironment).IsAssignableFrom(s.ServiceType));
 
         foreach (var descriptor in verifiableServices)
         {
